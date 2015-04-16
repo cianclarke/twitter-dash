06bf4d08509b3b25361dc04db326726991c7c5a2
cianclarke <cian@cianclarke.com>
2015-04-16 16:47:47 -0400

    Static tweet search of last 10

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var Twitter = require('twitter');
+ 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/search', function(req, res, next){
+ var hashtag = req.query.hashtag;
+ 
+ var client = new Twitter({
+   consumer_key: process.env.TWITTER_CONSUMER_KEY,
+   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+
+ });
+ client.get('search/tweets', {q: 'node.js'}, function(error, tweets, response){
+   return res.render('stream', { hashtag : hashtag, historic_tweets : tweets.statuses });
+ });
 });
 
 module.exports = router;
